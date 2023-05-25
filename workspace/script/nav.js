92ac32f990169f503d751808f5306df4f64f220b
Jejebal <96423022+Jejebal@users.noreply.github.com>
2023-05-25 12:53:19 +0200

    Navigation réparer

--- workspace/script/nav.js
+++ workspace/script/nav.js
@@ -1,6 +1,7 @@
 let loginLink = document.getElementById("loginLinkFhub");
 let loginPage = document.getElementById("loginPage");
 let hubPage = document.getElementById("hubPage");
+let hubLink = document.getElementById("hubLinkLogin");
 
 loginLink?.addEventListener("click",(e) => {
 
@@ -9,4 +10,13 @@ loginLink?.addEventListener("click",(e) => {
     loginPage.style.display = "block";
     hubPage.style.display = "none";
 
-})
+});
+
+hubLink?.addEventListener("click",(e) => {
+
+    e.preventDefault();
+
+    loginPage.style.display = "none";
+    hubPage.style.display = "block";
+
+});
