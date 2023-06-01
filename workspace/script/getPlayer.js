c48829e5decd8a95d0f6690dd08ee7a6cb9d8623
Jejebal <96423022+Jejebal@users.noreply.github.com>
2023-06-01 15:48:45 +0200

    Correction position démarrage

--- workspace/script/getPlayer.js
+++ workspace/script/getPlayer.js
@@ -1,3 +1,5 @@
+let position = hub.getBoundingClientRect()
+
 var myHeader = new Headers();
 
 var init = {
@@ -9,27 +11,39 @@ var init = {
 let data = await getPos()
 
 for (let i = 0; i < data["data"].length; i++) {
-    if (data["data"][i]["username"] != "dayan.mdx") {
-        let img = document.createElement("img")
+    let img = document.createElement("img")
     img.src = "./image/4.png"
     img.className = "other"
     img.id = data["data"][i]["username"]
     img.style.position = "absolute"
     document.getElementById("hub").appendChild(img)
-    }
-    
 }
 
 setInterval(async() => {
     data = await getPos()
     for (let i = 0; i < data["data"].length; i++) {
-        if (data["data"][i]["username"] != "dayan.mdx") {
-            let e = data["data"][i]
+        let e = data["data"][i]
         let img = document.getElementById(e["username"])
-        img.style.left = `${e["pos_x"]}px`
-        img.style.top = `${e["pos_y"]}px`
+        if (e["pos_x"] + position.left > position.right){
+
+            img.style.right = `${position.left}px`
+
         }
-        
+        else{
+
+            img.style.left = `${e["pos_x"] + position.left}px`
+
+        }
+        if (e["pos_y"] + position.top > position.bottom){
+
+            img.style.bottom = `${position.top}px`
+
+        }
+        else{
+
+            img.style.top = `${e["pos_y"] + position.top}px`
+
+        }
     }
 
 },500)
