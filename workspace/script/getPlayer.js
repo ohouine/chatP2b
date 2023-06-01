fc616db3a69b9caf2950c703aa8be56702181d88
Ohouine <dayan.mdx@eduge.ch>
2023-06-01 15:33:22 +0200

    kevin le gros mechant loup

--- workspace/script/getPlayer.js
+++ workspace/script/getPlayer.js
@@ -1,5 +1,3 @@
-let position = hub.getBoundingClientRect()
-
 var myHeader = new Headers();
 
 var init = {
@@ -11,21 +9,27 @@ var init = {
 let data = await getPos()
 
 for (let i = 0; i < data["data"].length; i++) {
-    let img = document.createElement("img")
+    if (data["data"][i]["username"] != "dayan.mdx") {
+        let img = document.createElement("img")
     img.src = "./image/4.png"
     img.className = "other"
     img.id = data["data"][i]["username"]
     img.style.position = "absolute"
     document.getElementById("hub").appendChild(img)
+    }
+    
 }
 
 setInterval(async() => {
     data = await getPos()
     for (let i = 0; i < data["data"].length; i++) {
-        let e = data["data"][i]
+        if (data["data"][i]["username"] != "dayan.mdx") {
+            let e = data["data"][i]
         let img = document.getElementById(e["username"])
-        img.style.left = `${e["pos_x"] + position.left}px`
-        img.style.top = `${e["pos_y"] + position.top}px`
+        img.style.left = `${e["pos_x"]}px`
+        img.style.top = `${e["pos_y"]}px`
+        }
+        
     }
 
 },500)
