d724aca4abf6c68cf989ff4434af1a88b14d5722
Ohouine <dayan.mdx@eduge.ch>
2023-06-01 15:00:58 +0200

    kev pas tres simpat

--- workspace/script/init.js
+++ workspace/script/init.js
@@ -1,9 +1,15 @@
 let loginP = document.getElementById("loginPage")
 let hubP = document.getElementById("hubPage")
+let lnklogin = document.getElementById("loginLinkFhub")
+let logout = document.getElementById("logout")
+
 
 if (localStorage.getItem("token") === null || localStorage.getItem("token") === "") {
     
     hubP.style.display = "none"
     loginP.style.display = "block";
 
+}else{
+    lnklogin.style.display = "none"
+    logout.style.display = "block"
 }
